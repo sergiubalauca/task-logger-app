23be0a54b281d7e6378fa1aa240ace28e78e074e
Sergiu <sergiu.balauca@gmail.com>
2024-02-03 12:34:47 +0200

    increases android version and adds t&c

--- src/app/shared/components/settings-popover/settings-popover.component.ts
+++ src/app/shared/components/settings-popover/settings-popover.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { IonicModule, PopoverController } from '@ionic/angular';
 import { NgIf } from '@angular/common';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
     selector: 'app-settings-popover',
@@ -9,13 +10,24 @@ import { NgIf } from '@angular/common';
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
     imports: [IonicModule, NgIf],
+    providers: [InAppBrowser],
 })
 export class SettingsPopoverComponent {
     public constructor(private popoverController: PopoverController) {}
-
+    private inAppBrowser: InAppBrowser = inject(InAppBrowser);
     public logout(): void {
         this.popoverController.dismiss({
-            logout: true
+            logout: true,
         });
+    }
+
+    protected openTermsAndConditions(): void {
+        const browser = this.inAppBrowser.create(
+            'https://sergiubalauca.github.io/dentalog-t-c/',
+            '_blank',
+            'presentationstyle=formsheet,toolbarposition=top,fullscreen=no,hideurlbar=yes,toolbarcolor=#176bff,closebuttoncolor=#ffffff,navigationbuttoncolor=#ffffff'
+        );
+
+        browser.show();
     }
 }
